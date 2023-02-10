72b416729205d7b3f8531b3cb3640c94413f78de
Flys3r <118739922+Flys3r@users.noreply.github.com>
2023-02-10 14:23:53 +0100

    Update facture.py

--- joli boite a code/facture.py
+++ joli boite a code/facture.py
@@ -7,23 +7,3 @@ from flask import render_template
 from flask_classful import FlaskView, route
 from forms import TitanicUserForm
 
-class DemoView(FlaskView):
-    def index(self):
-        html = "<a href='./titanic/add'> Add a record </a>"
-        return 'Hello World!<br>' + html
-
-    @route('/<name>')
-    def template(self, name):
-        return render_template('index.html', name=name)
-
-class TitanicView(FlaskView):
-
-    # Target : http://localhost:5000/titanic
-    def index(self):
-        return "<h1>Here we should list titanic records</h1>"
-
-    @route('/add')
-    def addTitanicRecord(self):
-        form = TitanicUserForm()
-        return render_template('titanic.html', form=form)
-

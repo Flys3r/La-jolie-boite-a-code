19ef2f9080c94d9ae65f09d6bdb95d50a474eceb
Jotaro15 <sterenn.alana@gmail.com>
2023-03-10 11:10:15 +0100

    tout

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import MySQLdb
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'jolieboite'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form')
+def formulaire():
+    return render_template('form.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    nomUtilisateur = request.form['nomUtilisateur']
+    mdp = request.form['mdp']
+    
+    # Save the data to the MySQL database
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # Sélectionnez la valeur maximale de idUtilisateur dans la table Utilisateur
+    cursor.execute('SELECT MAX(idUtilisateur) FROM Utilisateur')
+    result = cursor.fetchone()
+
+    # Si la table est vide, commencez à 1, sinon incrémentez la valeur maximale de idUtilisateur
+    if result[0] is None:
+        idUtilisateur = 1
+    else:
+        idUtilisateur = result[0] + 1
+
+    # Insérez une nouvelle ligne dans la table Utilisateur avec l'idUtilisateur auto-incrémenté
+    cursor.execute('INSERT INTO Utilisateur (idUtilisateur, nomUtilisateur, mdp) VALUES (%s, %s, %s)', (idUtilisateur, nomUtilisateur, mdp))
+    conn.commit()
+    conn.close()
+
+    return render_template('success.html', nomUtilisateur=nomUtilisateur, mdp=mdp)
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- joli boite a code/facture.py
+++
@@ -1,9 +0,0 @@
-"""
-Lis of views available for the titanic application.
-Stores also demo views simply for the pleasure of coding.
-"""
-
-from flask import render_template
-from flask_classful import FlaskView, route
-from forms import TitanicUserForm
-

9085f8023ec8a80684b9bff9b79ff788fd8fe763
Jotaro15 <sterenn.alana@gmail.com>
2023-03-10 13:38:19 +0100

    modification du html

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'jolieboite'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3307
 
 mysql = MySQL()
 mysql.init_app(app)
